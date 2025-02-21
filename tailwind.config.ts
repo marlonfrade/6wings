45365d7a20208f159fd4e4e5c5c3c454f6b83181
marlonfrade <marlonmelo98@hotmail.com>
2025-02-21 00:35:49 -0300

    style: add some extend configs for tailwind style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,106 +9,124 @@ const config = {
   ],
   prefix: '',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px'
-      }
-    },
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: '#9B297D',
-          50: '#F3E0EE',
-          100: '#E7C1DD',
-          200: '#D584BB',
-          300: '#C34799',
-          400: '#B13A8E',
-          500: '#9B297D',
-          600: '#85226B',
-          700: '#6F1C59',
-          800: '#591647',
-          900: '#431035',
-          950: '#360D2B'
-        },
-        secondary: {
-          DEFAULT: '#363C41',
-          50: '#D6D8D9',
-          100: '#BEC1C3',
-          200: '#8E9397',
-          300: '#5E656B',
-          400: '#464D53',
-          500: '#363C41',
-          600: '#2E3338',
-          700: '#262A2E',
-          800: '#1E2124',
-          900: '#16181A',
-          950: '#0F1012'
-        },
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))'
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))'
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))'
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))'
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))'
-        }
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' }
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' }
-        }
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
-      },
-      letterSpacing: {
-        tightest: '-.075em',
-        tighter: '-.05em',
-        tight: '-.025em',
-        normal: '0',
-        wide: '.025em',
-        wider: '.05em',
-        widest: '.1em'
-      },
-      lineHeight: {
-        none: '1',
-        tight: '1.25',
-        snug: '1.375',
-        normal: '1.5',
-        relaxed: '1.625',
-        loose: '2'
-      }
-    }
+  	container: {
+  		center: true,
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	extend: {
+  		colors: {
+  			primary: {
+  				'50': '#F3E0EE',
+  				'100': '#E7C1DD',
+  				'200': '#D584BB',
+  				'300': '#C34799',
+  				'400': '#B13A8E',
+  				'500': '#9B297D',
+  				'600': '#85226B',
+  				'700': '#6F1C59',
+  				'800': '#591647',
+  				'900': '#431035',
+  				'950': '#360D2B',
+  				DEFAULT: '#9B297D'
+  			},
+  			secondary: {
+  				'50': '#D6D8D9',
+  				'100': '#BEC1C3',
+  				'200': '#8E9397',
+  				'300': '#5E656B',
+  				'400': '#464D53',
+  				'500': '#363C41',
+  				'600': '#2E3338',
+  				'700': '#262A2E',
+  				'800': '#1E2124',
+  				'900': '#16181A',
+  				'950': '#0F1012',
+  				DEFAULT: '#363C41'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		},
+  		letterSpacing: {
+  			tightest: '-.075em',
+  			tighter: '-.05em',
+  			tight: '-.025em',
+  			normal: '0',
+  			wide: '.025em',
+  			wider: '.05em',
+  			widest: '.1em'
+  		},
+  		lineHeight: {
+  			none: '1',
+  			tight: '1.25',
+  			snug: '1.375',
+  			normal: '1.5',
+  			relaxed: '1.625',
+  			loose: '2'
+  		}
+  	}
   },
   plugins: [require('tailwindcss-animate')],
   variants: {
