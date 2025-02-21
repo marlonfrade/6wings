8d66832e5815b1d5effbee20c18eb2cebe296a1e
marlonfrade <marlonmelo98@hotmail.com>
2025-02-21 00:41:23 -0300

    feat: create basic home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { AssistantRuntimeProvider } from '@assistant-ui/react'
 import { useChatRuntime } from '@assistant-ui/react-ai-sdk'
 import { AssistantModal } from '@/components/assistant-ui/assistant-modal'
+import { Navbar } from '@/components/navbar'
 
 export default function Home() {
   const runtime = useChatRuntime({
@@ -11,9 +12,12 @@ export default function Home() {
 
   return (
     <AssistantRuntimeProvider runtime={runtime}>
-      <div className='grid h-dvh grid-cols-[200px_1fr] gap-x-2 px-4 py-4'>
-        <AssistantModal />
-      </div>
+      <main>
+        <Navbar />
+        <div className="grid h-dvh grid-cols-[200px_1fr] gap-x-2 px-4 py-4">
+          <AssistantModal />
+        </div>
+      </main>
     </AssistantRuntimeProvider>
   )
 }
