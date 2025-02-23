3185107a648d597a1580d1e765dd1f65d7842cab
marlonfrade <marlonmelo98@hotmail.com>
2025-02-23 11:41:47 -0300

    feat: add dedicated local fonts configuration

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,47 +1,30 @@
 import localFont from 'next/font/local'
 
-// Configure local Montserrat font
+// Configure Montserrat font
 export const montserrat = localFont({
   src: [
     {
-      path: '/fonts/Montserrat/Montserrat-Light.ttf',
-      weight: '300',
-      style: 'normal'
-    },
-    {
-      path: '/fonts/Montserrat/Montserrat-Regular.ttf',
+      path: '/fonts/Montserrat/static/Montserrat-Regular.ttf',
       weight: '400',
       style: 'normal'
     },
     {
-      path: '/fonts/Montserrat/Montserrat-Medium.ttf',
+      path: '/fonts/Montserrat/static/Montserrat-Medium.ttf',
       weight: '500',
       style: 'normal'
     },
     {
-      path: '/fonts/Montserrat/Montserrat-SemiBold.ttf',
-      weight: '600',
-      style: 'normal'
-    },
-    {
-      path: '/fonts/Montserrat/Montserrat-Bold.ttf',
+      path: '/fonts/Montserrat/static/Montserrat-Bold.ttf',
       weight: '700',
       style: 'normal'
     }
   ],
-  display: 'swap',
-  variable: '--font-montserrat',
-  fallback: ['system-ui', 'arial']
+  variable: '--font-montserrat'
 })
 
-// Configure local Prompt font
+// Configure Prompt font
 export const prompt = localFont({
   src: [
-    {
-      path: '/fonts/Prompt/Prompt-Light.ttf',
-      weight: '300',
-      style: 'normal'
-    },
     {
       path: '/fonts/Prompt/Prompt-Regular.ttf',
       weight: '400',
@@ -53,17 +36,10 @@ export const prompt = localFont({
       style: 'normal'
     },
     {
-      path: '/fonts/Prompt/Prompt-SemiBold.ttf',
-      weight: '600',
-      style: 'normal'
-    },
-    {
       path: '/fonts/Prompt/Prompt-Bold.ttf',
       weight: '700',
       style: 'normal'
     }
   ],
-  display: 'swap',
-  variable: '--font-prompt',
-  fallback: ['system-ui', 'arial']
+  variable: '--font-prompt'
 })
